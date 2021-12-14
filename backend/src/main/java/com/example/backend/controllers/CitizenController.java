53964cd2befac9f4add22319b2f9df6555ce431b
Marko Vranjes <marko.vranjes99@gmail.com>
2021-12-14 10:21:39 +0100

    post request citizens

--- backend/src/main/java/com/example/backend/controllers/CitizenController.java
+++ backend/src/main/java/com/example/backend/controllers/CitizenController.java
@@ -3,9 +3,8 @@ package com.example.backend.controllers;
 import com.example.backend.models.entities.CitizenEntity;
 import com.example.backend.repositories.CitizenEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,4 +23,8 @@ public class CitizenController {
     {
         return citizenEntityRepository.findAll();
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    CitizenEntity save(@RequestBody CitizenEntity citizen) { return citizenEntityRepository.save(citizen); }
 }
