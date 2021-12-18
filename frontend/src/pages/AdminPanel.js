15cfa7784b3f69f0da51e2549ac95df548415338
jelenamackic <mackicj@gmail.com>
2021-12-18 18:05:40 +0100

    dizajn tabele je zavrsen, preostaje samo paginacija

--- frontend/src/pages/AdminPanel.js
+++ frontend/src/pages/AdminPanel.js
@@ -4,19 +4,31 @@ import "antd/dist/antd.css";
 import {Button, Input, Table} from 'antd';
 import styles from "../styles/adminPanel.css"
 import Icon, {SearchOutlined} from "@ant-design/icons";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 import axios from 'axios'
 
-const token = "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiIzIiwic3ViIjoiYWRtaW4iLCJyb2xlIjoiQURNSU4iLCJleHAiOjE2Mzk3NTg5MzR9.z6xqzeVWdE9TUZKTdI5CxXFFCLVWYnTkI3x6nzyJMqfWNukjDMIKF278SY01s87T2xdNdvH5jGvLCil8CkQbdQ";
+const token = "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiIzIiwic3ViIjoiYWRtaW4iLCJyb2xlIjoiQURNSU4iLCJleHAiOjE2Mzk4NDY0Mjl9.hUYOvQ8yiM6NV3i3MzA5zClIuFHQ9BjeRUqCNrkERIlNC95-SFtyWcV4cekLUvLB8BCxs2bYuMdUc7DhZaLWCg";
+
+
 
 
 
 const AdminPanel = () => {
 
     const [data, setData] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [pageSize, setPageSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
+    const [itemsPerPage, setItemsPerPage] = useState(10);
+
     const fetchData = async () => {
         try {
             const result = await axios.get("/citizens", {headers: {"Authorization": `Bearer ${token}`}});
             setData(result.data);
+            setIsLoaded(true);
             console.log(result.data);
         } catch (err) {
             console.log(err);
@@ -24,23 +36,39 @@ const AdminPanel = () => {
     };
 
     useEffect(() => {
+        const handleResize = () => {
+            setPageSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.addEventListener("resize", handleResize);
         fetchData();
+        return () => window.removeEventListener("resize", handleResize);
+
     }, []);
 
+    useEffect(()=>{
+        if(pageSize.width <= 768){
+            setItemsPerPage(5);
+        }else{
+            setItemsPerPage(10);
+        }
+    },[pageSize]);
 
     const columns = [
         {
             title: 'Ime i prezime',
             dataIndex: 'name',
             key: 'name',
-            render: (text, record) => <p> {record.name} {record.lastname}</p>,
+            render: (text, record) => <p> {record.firstname} {record.lastname}</p>,
 
 
         },
         {
             title: 'Godine starosti',
-            dataIndex: 'year',
-            key: 'year',
+            dataIndex: 'year_of_birth',
+            key: 'year_of_birth',
             width: 15,
         },
         {
@@ -53,16 +81,19 @@ const AdminPanel = () => {
             title: 'Email',
             dataIndex: 'email',
             key: 'email',
+            className:"email-column"
+            // render: (text) => <p className={"email-column"}>{text}</p>,
         },
         {
             title: 'Telefon',
             dataIndex: 'phone',
             key: 'phone',
+            width: 20,
         },
         {
             title: 'Strucna sprema',
-            dataIndex: 'citizencol',
-            key: 'citizencol',
+            dataIndex: 'education',
+            key: 'education',
         },
         {
             title: 'Radno mjesto',
@@ -76,13 +107,14 @@ const AdminPanel = () => {
         },
         {
             title: 'Drzavljanstvo',
-            dataIndex: ['citizenshipEntity','country'],
+            dataIndex: ['citizenshipEntity', 'country'],
             key: 'country',
+            width: 20,
         },
         {
             title: 'Godina dolaska ',
-            dataIndex: 'comingYear',
-            key: 'comingYear',
+            dataIndex: 'year_of_arrival',
+            key: 'year_of_arrival',
             width: 15,
         },
 
@@ -90,7 +122,7 @@ const AdminPanel = () => {
             title: 'Broj clanova domacinstva',
             dataIndex: 'num_of_family_members',
             key: 'num_of_family_members',
-            width: 15,
+            // width: 15,
         }
     ];
 
@@ -102,18 +134,32 @@ const AdminPanel = () => {
         console.log('Kliknuto je na dugme', e);
     }
 
+    if(!isLoaded){
+        return (
+            <Loader>
+                Loading
+            </Loader>
+        )
+    }
 
     return (
-        <div className="AdminPanel">
-            <div className= "table-component">
+        <div className="admin-panel">
+            <div className="table-component">
                 <Table className="table"
-                        pagination={{pageSize: 7}}
-                        dataSource={data}
-                        columns={columns}
-                        onRow={record => ({
+                       pagination={
+                           {
+                               pageSize: itemsPerPage,
+                               position: ["bottomCenter"]
+                           }
+
+                       }
+                       dataSource={data}
+                       columns={columns}
+                       onRow={record => ({
                            onClick: (e) => onSelect(e)
-                        })}
-                        rowClassName={"rows"}
+                       })}
+                       rowClassName={"rows"}
+                       size={"small"}
                 />
             </div>
 
