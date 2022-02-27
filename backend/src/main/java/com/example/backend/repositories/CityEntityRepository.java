06ab70ff1ed617135259fe6f7427ca340cc318a8
Marko Vranjes <marko.vranjes99@gmail.com>
2022-02-27 22:23:15 +0100

    city controller and repository added

---
+++ backend/src/main/java/com/example/backend/repositories/CityEntityRepository.java
@@ -0,0 +1,9 @@
+package com.example.backend.repositories;
+
+import com.example.backend.models.entities.CitizenEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityEntityRepository extends JpaRepository<CitizenEntity,Integer> {
+}
