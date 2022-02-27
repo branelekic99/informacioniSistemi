29da59dee44ba0bb45fcfdd1c6f3a8132c767ef7
Marko Vranjes <marko.vranjes99@gmail.com>
2022-02-27 23:34:12 +0100

    added cities.json file to git

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,14 +1,31 @@
 package com.example.backend;
 
+import com.example.backend.models.entities.CityEntity;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.*;
+
 @SpringBootApplication
 public class BackendApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        /*Gson gson = new Gson();
+        FileWriter fw = new FileWriter("slo_cities.json");
+        fw.write("[\n");
+        JsonReader citiesJson = new JsonReader(new FileReader("cities.json"));
+        CityEntity[] cities = gson.fromJson(citiesJson, CityEntity[].class);
+        int i = 0;
+        for(CityEntity city : cities){
+            if(city.getCountry().equals("SI")){
+                fw.write(city.toString());
+            }
+        }
+        fw.write("\n]");*/
         SpringApplication.run(BackendApplication.class, args);
     }
 
